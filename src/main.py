a376a67a25961016f602556b966a982877779da8
nickmath <113207847+nickmath29@users.noreply.github.com>
2024-12-22 10:56:10 -0500

    added board

--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
+import pygame
+import sys
+from const import *
+
 
 class Main:
     
     def __init__(self):
-        pass
+        #print('init')
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Chess')
+        
+        
     def mainloop(self):
-        pass
-     
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                    
+                    
+            pygame.display.update()
+    
+    
 main = Main()
 main.mainloop()
